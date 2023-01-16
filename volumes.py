cd7ec6f343763e224b5a50ff3bc023c24464f30a
joycerijs <j.rijs@student.tudelft.nl>
2023-01-16 16:56:07 +0100

    laatste line txt file printen

--- volumes.py
+++ volumes.py
@@ -1,7 +1,18 @@
 '''Read freesurfer .stats files of subcortical and cortical structures'''
 
+import pandas as pd
+
 subcortical_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/aseg.stats")
 cortex_rh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/rh.aparc.DKTatlas.stats")
 cortex_lh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/lh.aparc.DKTatlas.stats")
 
-print(subcortical_file.read())
+# print(subcortical_file.readline(65))
+
+# laatste line txt file printen:
+
+# with open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/aseg.stats") as f:
+#     for line in f:
+#         pass
+#     last_line = line
+
+# print(last_line)
