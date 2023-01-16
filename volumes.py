2ffa187719ea0f4cda14eddded671b63bdab04db
joycerijs <j.rijs@student.tudelft.nl>
2023-01-16 15:58:27 +0100

    open stats files

---
+++ volumes.py
@@ -0,0 +1,5 @@
+'''Read freesurfer .stats files of subcortical and cortical structures'''
+
+subcortical_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/aseg.stats")
+cortex_rh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/rh.aparc.DKTatlas.stats")
+cortex_lh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/lh.aparc.DKTatlas.stats")
