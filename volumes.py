e9f98b2e9d3565e7fbb0e62d7b57960d2bdd3516
joycerijs <j.rijs@student.tudelft.nl>
2023-01-16 16:00:45 +0100

    read file

--- volumes.py
+++ volumes.py
@@ -3,3 +3,5 @@
 subcortical_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/aseg.stats")
 cortex_rh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/rh.aparc.DKTatlas.stats")
 cortex_lh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/lh.aparc.DKTatlas.stats")
+
+print(subcortical_file.read())
