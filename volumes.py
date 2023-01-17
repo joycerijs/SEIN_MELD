6358da99ba332d1fc46ae3140ce917e66691c8ae
joycerijs <j.rijs@student.tudelft.nl>
2023-01-17 15:41:25 +0100

    Create dataframes of left and right struct volumes

--- volumes.py
+++ volumes.py
@@ -6,13 +6,16 @@ subcortical_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/aseg.
 cortex_rh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/rh.aparc.DKTatlas.stats")
 cortex_lh_file = open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/lh.aparc.DKTatlas.stats")
 
-# print(subcortical_file.readline(65))
+# print(subcortical_file.read())
 
-# laatste line txt file printen:
+volume_dataframe = pd.read_table(subcortical_file, delim_whitespace=True, header=None, comment='#', usecols=[3, 4], names=['Volume in mm3', 'Name of structure'])
+# comment = '#' geeft aan dat de lijnen met # overgeslagen worden.
+columns = volume_dataframe.columns.tolist()
+columns = columns[-1:] + columns[:-1]
+volume_dataframe = volume_dataframe[columns]
 
-# with open("C:/Users/joycerijs/freesurfer/subjects/bert/stats/aseg.stats") as f:
-#     for line in f:
-#         pass
-#     last_line = line
+left_structures = volume_dataframe[volume_dataframe['Name of structure'].str.contains('Left')]
+right_structures = volume_dataframe[volume_dataframe['Name of structure'].str.contains('Right')]
 
-# print(last_line)
+# print(left_structures)
+# print(right_structures)
