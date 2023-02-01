bc7044c10df13e6c73f8577943f849512963808e
joycerijs <j.rijs@student.tudelft.nl>
2023-02-01 15:56:51 +0100

    Create confidence ellipse

--- volumes.py
+++ volumes.py
@@ -1,14 +1,66 @@
-'''Read freesurfer .stats files of subcortical and cortical structures'''
+'''Read freesurfer .stats files of subcortical structures and create volume scatterplots with confidence ellipse'''
 
 import pandas as pd
-import os
 from os import listdir
 from os.path import isfile, join
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 import numpy as np
-import scipy as sp
-import scipy.stats as stats
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensional dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the standard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the standard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
 
 # Load file of current patient
 file_path = "C:/Users/joycerijs/freesurfer/subjects/bert/stats/aseg.stats"
@@ -30,10 +82,9 @@ bert_volume_norm = bert_volume_nor.multiply(1000, axis=0)
 path = "F:/Documenten/Universiteit/Master_TM+_commissies/Jaar 2/Stages/Stage 4/Bestanden voor project/map18 stats/volumestats/"
 subcortical_files = [f for f in listdir(path) if isfile(join(path, f))]
 
-# print(subcortical_file.read())
+# Create empty lists
 values_list = []
 names_list = []
-
 icv_values = []
 names_icv_values = []
 structures = []
@@ -67,132 +118,16 @@ for k in range(len(structures_of_interest)):
     plt.ylabel(f'Volume right {structures_of_interest[k]} (mL)')
     min_value = (volume_norm[[f'Left-{structures_of_interest[k]}', f'Right-{structures_of_interest[k]}']].min(axis=1)).min(axis=0)
     max_value = (volume_norm[[f'Left-{structures_of_interest[k]}', f'Right-{structures_of_interest[k]}']].max(axis=1)).max(axis=0)
+    confidence_ellipse(volume_norm[f'Left-{structures_of_interest[k]}'], volume_norm[f'Right-{structures_of_interest[k]}'], ax, n_std=2.0, label=r'$2\sigma$', edgecolor='blue', linestyle=':')
+    plt.legend()
     plt.xlim(min_value-0.25, max_value+0.25)
     plt.ylim(min_value-0.25, max_value+0.25)
-    # Linear line
-    # plt.plot([min_value-0.25, max_value+0.25], [min_value-0.25, max_value+0.25], linestyle="--")
-    # create linear regression
-    a, b = np.polyfit(volume_norm[f'Left-{structures_of_interest[k]}'], volume_norm[f'Right-{structures_of_interest[k]}'], 1)
-    xs = np.linspace(min_value-0.25, max_value+0.25)
-    plt.plot(xs, a*xs+b, c='k', lw=2)
-    ci = 0.95 * np.std(a*xs+b) / np.mean(a*xs+b) # Dit nog netter maken. volgens mij klopt het niet helemaal
-    plt.fill_between(xs, (a*xs+b-ci), (a*xs+b+ci), color='blue', alpha=0.2)
-    plt.legend()
     # plt.savefig(join(path, f'Left vs. Right-{structures_of_interest[k]} (ICV-normalized).jpg'))
     plt.show()
     plt.clf()
 
-print('finish')
-# # Van stack overflow, werkt nog niet: https://stackoverflow.com/questions/27164114/show-confidence-limits-and-prediction-limits-in-scatter-plot
 
-# def plot_ci_manual(t, s_err, n, x, x2, y2, ax=None):
-#     """Return an axes of confidence bands using a simple approach.
-    
-#     Notes
-#     -----
-#     .. math:: \left| \: \hat{\mu}_{y|x0} - \mu_{y|x0} \: \right| \; \leq \; T_{n-2}^{.975} \; \hat{\sigma} \; \sqrt{\frac{1}{n}+\frac{(x_0-\bar{x})^2}{\sum_{i=1}^n{(x_i-\bar{x})^2}}}
-#     .. math:: \hat{\sigma} = \sqrt{\sum_{i=1}^n{\frac{(y_i-\hat{y})^2}{n-2}}}
-    
-#     References
-#     ----------
-#     .. [1] M. Duarte.  "Curve fitting," Jupyter Notebook.
-#        http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/CurveFitting.ipynb
-    
-#     """
-#     if ax is None:
-#         ax = plt.gca()
-    
-#     ci = t * s_err * np.sqrt(1/n + (x2 - np.mean(x))**2 / np.sum((x - np.mean(x))**2))
-#     ax.fill_between(x2, y2 + ci, y2 - ci, color="#b9cfe7", edgecolor="")
-
-#     return ax
-
-
-# def plot_ci_bootstrap(xs, ys, resid, nboot=500, ax=None):
-#     """Return an axes of confidence bands using a bootstrap approach.
-
-#     Notes
-#     -----
-#     The bootstrap approach iteratively resampling residuals.
-#     It plots `nboot` number of straight lines and outlines the shape of a band.
-#     The density of overlapping lines indicates improved confidence.
-
-#     Returns
-#     -------
-#     ax : axes
-#         - Cluster of lines
-#         - Upper and Lower bounds (high and low) (optional)  Note: sensitive to outliers
-
-#     References
-#     ----------
-#     .. [1] J. Stults. "Visualizing Confidence Intervals", Various Consequences.
-#        http://www.variousconsequences.com/2010/02/visualizing-confidence-intervals.html
-
-#     """ 
-#     if ax is None:
-#         ax = plt.gca()
-
-#     bootindex = sp.random.randint
-
-#     for _ in range(nboot):
-#         resamp_resid = resid[bootindex(0, len(resid) - 1, len(resid))]
-#         # Make coeffs of for polys
-#         pc = sp.polyfit(xs, ys + resamp_resid, 1)                   
-#         # Plot bootstrap cluster
-#         ax.plot(xs, sp.polyval(pc, xs), "b-", linewidth=2, alpha=3.0 / float(nboot))
-
-#     return ax
-
-# # Computations ----------------------------------------------------------------    
-# # Modeling with Numpy
-# def equation(a, b):
-#     """Return a 1D polynomial."""
-#     return np.polyval(a, b) 
-
-
-# x = volume_norm['Left-Hippocampus']
-# y = volume_norm['Right-Hippocampus']
-# p, cov = np.polyfit(x, y, 1, cov=True)                     # parameters and covariance from of the fit of 1-D polynom.
-# y_model = equation(p, x)                                   # model using the fit parameters; NOTE: parameters here are coefficients
-
-# # Statistics
-# n = volume_norm['Left-Hippocampus'].size                    # number of observations
-# m = p.size                                                 # number of parameters
-# dof = n - m                                                # degrees of freedom
-# t = stats.t.ppf(0.975, n - m)                              # t-statistic; used for CI and PI bands
-
-# # Estimates of Error in Data/Model
-# resid = y - y_model                                        # residuals; diff. actual data from predicted values
-# chi2 = np.sum((resid / y_model)**2)                        # chi-squared; estimates error in data
-# chi2_red = chi2 / dof                                      # reduced chi-squared; measures goodness of fit
-# s_err = np.sqrt(np.sum(resid**2) / dof)                    # standard deviation of the error
-
-# # Plotting --------------------------------------------------------------------
-# fig, ax = plt.subplots(figsize=(8, 6))
-
-# # Data
-# ax.plot(
-#     x, y, "o", color="#b9cfe7", markersize=8, 
-#     markeredgewidth=1, markeredgecolor="b", markerfacecolor="None"
-# )
-
-# # Fit
-# ax.plot(x, y_model, "-", color="0.1", linewidth=1.5, alpha=0.5, label="Fit")  
-
-# x2 = np.linspace(np.min(x), np.max(x), 100)
-# y2 = equation(p, x2)
-
-# # Confidence Interval (select one)
-# plot_ci_manual(t, s_err, n, x, x2, y2, ax=ax)
-# #plot_ci_bootstrap(x, y, resid, ax=ax)
-   
-# # Prediction Interval
-# pi = t * s_err * np.sqrt(1 + 1/n + (x2 - np.mean(x))**2 / np.sum((x - np.mean(x))**2))   
-# ax.fill_between(x2, y2 + pi, y2 - pi, color="None", linestyle="--")
-# ax.plot(x2, y2 - pi, "--", color="0.5", label="95% Prediction Limits")
-# ax.plot(x2, y2 + pi, "--", color="0.5")
-# plt.show()
-
+# print('finish')
 
 # # # test = (open(join(path, subcortical_files[0])))
 # # # print(test.read())
